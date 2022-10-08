832e2c30ba36edbf3023577aa2fb3d544e403de8
YuHuiLi2026 <111806126+YuHuiLi2026@users.noreply.github.com>
2022-10-07 23:07:34 -0700

    Comment for Part 3

---
+++ Part3.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+int main(int argc, char* argv[]) {
+  std::vector<std::string> arguments(argv, argv + argc);
+  // TO-DO create a bool variable in order to skip the first element in the for-each loop
+  
+  // TO-DO create an int variable in order to store the final score 
+  
+  // TO-DO create a for-each loop to iterate every element in the arguments
+  // In order to pass the test, you need to using const and & in the loop
+  // Example : for(const std::string& s : arguments) 
+  
+  // TO-DO Inside the for-each loop, use if else Statement to skip the first element of arguments
+     if(skip) {
+      skip = false; 
+     } else {
+       // TO-DO create a if statement, when input element is 2 to 10
+       // 
+       // Use std::stoi to change the string element to a int type 
+       // Then store the value into final score (created in line 8) 
+       //
+       
+       // TO-DO create a else-if, when input element is JQK
+       // Use final Score += 10 to store the value
+       
+       // TO-Do create another else-if, when input element is A
+       // Inside this else-if, create a if and else : if{}else{} not else if{}
+       // 2 cases: 
+       // When 'A' add other card is smaller or equal to 21, add 11
+       // 11 can only be added once to score
+       
+       // Try to think this in another way: smaller or equal(<=) 21 means before add A to the score
+       // The score should be smaller or equal to 10
+       // 
+       // else the A should count 1 to the score
+       
+       // TO-DO After two else-if statement, create a else statement
+       // This is used for when the input is not 2 to 10, JQK or A
+       // print out error: unknown card : 
+       // 
+     }
+       // TO-DO 
+       // Create a if else statement
+       // When Score is less than 21 
+       // Print out final score 
+  
+       // else
+       // When Score is greater than 21
+       // Print out final score, BUST
+  
+       return 0;
+}
+       
+       
